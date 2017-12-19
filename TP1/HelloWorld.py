305a5a026a9019e4dbd872d7d56f2554a991be2e
Khaloudinho <khaled.bouguettoucha5@etu.univ-lorraine.fr>
2017-12-19 20:06:17 +0100

    HelloWorld done

---
+++ TP1/HelloWorld.py
@@ -0,0 +1,18 @@
+# Input : 42 51
+# Output : 43 45 47 49 51
+
+def get_odd_numbers_between_two_numbers(nb1, nb2):
+    output = []
+
+    if nb1 % 2 == 0:
+        nb1 += 1
+
+    while nb1 <= nb2:
+        output.append(nb1)
+        nb1 += 2
+
+    return output
+
+print(get_odd_numbers_between_two_numbers(4, 13))
+
+
